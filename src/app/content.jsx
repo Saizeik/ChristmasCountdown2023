755ab54a32ff94fd6b21672c50a4b0482e4b9c83
Nathan Walker <nswalker44@gnail.com>
2023-11-26 12:32:46 -0800

    css update

--- src/app/content.jsx
+++ src/app/content.jsx
@@ -7,31 +7,23 @@ import christmasTree from "./christmas-tree.png";
 export default function getContent() {
   return (
     <div className="content-container">
-      <div className= 'animate__animated animate__shakeX  jingleBells-container'>
-        <Image
-          className="jingleBells-image"
-          src={jingleBells}
-          alt="jingle Bells"
-        />
-        <Image
-          className="jingleBells-image"
-          src={jingleBells}
-          alt="jingle Bells"
-        />
-      </div>
+ 
       <h1> Christmas Countdown </h1>{" "}
       <div className="cover"></div>
       <div className="christmas-tree-container">
       
       
         <Image
-          className="animate__animated animate__rubberBand christmas-tree"
+          className={`animate__animated animate__rubberBand christmas-tree`}
           src={christmasTree}
         />
+     
+        
         <div className="snow"></div>
         
         
       </div>
+      <div class="legs-feet"></div>
       <div className="bottom"> <div class="bt1"></div>
     <div className="bt2"></div></div>
     </div>
